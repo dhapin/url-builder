187e7dbb3b5295225c7dd0561bad93989761e242
dhapin <67732961+dhapin@users.noreply.github.com>
2020-10-02 07:24:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validateEmail(email) {
- if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
-  {
+  if (email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
     return (true);
-  }
+  } else {
     return (false);
+  }
 }
