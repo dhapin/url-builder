719916742d4a04eb3233941e17f624eb0e1c5df1
dhapin <67732961+dhapin@users.noreply.github.com>
2020-10-02 07:21:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-document.body.addEventListener('resize', () => {
-});
+function validateEmail(email) {
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
+  {
+    return (true);
+  }
+    return (false);
+}
