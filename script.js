ffc57fd049449e0607d39411e5dd6d8b9f4c864c
dhapin <67732961+dhapin@users.noreply.github.com>
2020-10-03 06:48:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-$('#input-url').on('input', () => {
+$('#input-url').on('input blur', () => {
   if ($('#input-url').val() != '') {
     $('#input-url').addClass('input-success').removeClass('input-danger');
   }
 });
 
-$('#input-source').on('input', () => {
+$('#input-source').on('input blur', () => {
   if ($('#input-source').val() != '') {
     $('#input-source').addClass('input-success').removeClass('input-danger');
   }
 });
 
-$('#input-team').on('input', () => {
+$('#input-team').on('input blur', () => {
   if ($('#input-team').val() != '' && validateEmail($('#input-team').val())) {
     $('#input-team').addClass('input-success').removeClass('input-danger');
+  }
+});
+
+$('#input-url, #input-source, #input-team').on('input blur', () => {
+  if ($('#input-url').hasClass('input-success') && $('#input-source').hasClass('input-success') && $('#input-team').hasClass('input-success')) {
+    $('#generated-url').val($('#input-url') + "#src=" + encodeURIComponent($('#input-source')) + "&team=" + encodeURIComponent($('#input-team')));
   }
 });
 
