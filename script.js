21c03732671f204f64c14c92d9bd6c29858c295e
dhapin <67732961+dhapin@users.noreply.github.com>
2020-10-02 17:55:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ $('#input-url').on('input', () => {
     $('#input-url').addClass('input-success').removeClass('input-danger');
   }
 });
+
+$('#input-source').on('input', () => {
+  if ($('#input-url').val() != '') {
+    $('#input-url').addClass('input-success').removeClass('input-danger');
+  }
+});
+
+$('#input-team').on('input', () => {
+  if ($('#input-url').val() != '') {
+    $('#input-url').addClass('input-success').removeClass('input-danger');
+  }
+});
+
 function validateEmail(email) {
   if (email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
     return (true);
